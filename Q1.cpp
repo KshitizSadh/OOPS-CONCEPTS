8019e0ad4ce2fa14a9bf4385a3236d1cd019cc1b
Kshitiz Dev Sadh <sadhshitiz@gmail.com>
2024-05-14 13:13:03 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 
 double computeSeriesSum(int n) {
     double sum = 0.0;
@@ -18,17 +19,17 @@ int main(int argc, char *argv[]) {
     if (argc > 1) {
         n = std::stoi(argv[1]);
     } else {
-        std::cout << "Enter the value of n: ";
-        std::cin >> n;
+        cout << "Enter the value of n: ";
+        cin >> n;
     }
 
     if (n <= 0) {
-        std::cerr << "Error: Invalid value of n. Please enter a positive integer.\n";
+        cerr << "Error: Invalid value of n. Please enter a positive integer.\n";
         return 1;
     }
 
     double sum = computeSeriesSum(n);
-    std::cout << "Sum of the first " << n << " terms of the series: " << sum << std::endl;
+    cout << "Sum of the first " << n << " terms of the series: " << sum << std::endl;
 
     return 0;
 }
